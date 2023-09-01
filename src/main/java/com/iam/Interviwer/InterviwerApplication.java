adc7436c6a90a51ea7f1f05af675c520d7f9bfc1
sagar <sagarbramhnote@gmail.com>
2023-09-01 17:48:41 +0530

    y

---
+++ src/main/java/com/iam/Interviwer/InterviwerApplication.java
@@ -0,0 +1,13 @@
+package com.iam.Interviwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviwerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iam/Interviwer/InterviwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.Interviwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
